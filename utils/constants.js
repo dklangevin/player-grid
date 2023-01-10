9ff2c2c6ddfb6015f00846b9886c3b91c49d83dc
Danny Langevin <dklangevin@gmail.com>
2023-01-09 18:32:34 -0700

    Add constants file

---
+++ utils/constants.js
@@ -0,0 +1,193 @@
+export const logos = {
+  'Atlanta Hawks': 'https://www.nba.com/assets/logos/teams/primary/web/ATL.svg',
+  'Boston Celtics':
+    'https://www.nba.com/assets/logos/teams/primary/web/BOS.svg',
+  'Brooklyn Nets': 'https://www.nba.com/assets/logos/teams/primary/web/BKN.svg',
+  'Charlotte Hornets':
+    'https://www.nba.com/assets/logos/teams/primary/web/CHA.svg',
+  'Chicago Bulls': 'https://www.nba.com/assets/logos/teams/primary/web/CHI.svg',
+  'Cleveland Cavaliers':
+    'https://www.nba.com/assets/logos/teams/primary/web/CLE.svg',
+  'Dallas Mavericks':
+    'https://www.nba.com/assets/logos/teams/primary/web/DAL.svg',
+  'Denver Nuggets':
+    'https://www.nba.com/assets/logos/teams/primary/web/DEN.svg',
+  'Detroit Pistons':
+    'https://www.nba.com/assets/logos/teams/primary/web/DET.svg',
+  'Golden State Warriors':
+    'https://www.nba.com/assets/logos/teams/primary/web/GSW.svg',
+  'Houston Rockets':
+    'https://www.nba.com/assets/logos/teams/primary/web/HOU.svg',
+  'Indiana Pacers':
+    'https://www.nba.com/assets/logos/teams/primary/web/IND.svg',
+  'LA Clippers': 'https://www.nba.com/assets/logos/teams/primary/web/LAC.svg',
+  'Los Angeles Lakers':
+    'https://www.nba.com/assets/logos/teams/primary/web/LAL.svg',
+  'Memphis Grizzlies':
+    'https://www.nba.com/assets/logos/teams/primary/web/MEM.svg',
+  'Miami Heat': 'https://www.nba.com/assets/logos/teams/primary/web/MIA.svg',
+  'Milwaukee Bucks':
+    'https://www.nba.com/assets/logos/teams/primary/web/MIL.svg',
+  'Minnesota Timberwolves':
+    'https://www.nba.com/assets/logos/teams/primary/web/MIN.svg',
+  'New Orleans Pelicans':
+    'https://www.nba.com/assets/logos/teams/primary/web/NOP.svg',
+  'New York Knicks':
+    'https://www.nba.com/assets/logos/teams/primary/web/NYK.svg',
+  'Oklahoma City Thunder':
+    'https://www.nba.com/assets/logos/teams/primary/web/OKC.svg',
+  'Orlando Magic': 'https://www.nba',
+};
+
+export const teamsData = [
+  {
+    name: 'Atlanta Hawks',
+    primaryColor: '#E03A3E',
+    secondaryColor: '#C1D32F',
+  },
+  {
+    name: 'Boston Celtics',
+    primaryColor: '#007A33',
+    secondaryColor: '#BA9653',
+  },
+  {
+    name: 'Brooklyn Nets',
+    primaryColor: '#000000',
+    secondaryColor: '#FFFFFF',
+  },
+  {
+    name: 'Charlotte Hornets',
+    primaryColor: '#008CA8',
+    secondaryColor: '#1D1160',
+  },
+  {
+    name: 'Chicago Bulls',
+    primaryColor: '#CE1141',
+    secondaryColor: '#000000',
+  },
+  {
+    name: 'Cleveland Cavaliers',
+    primaryColor: '#6F263D',
+    secondaryColor: '#041E42',
+  },
+  {
+    name: 'Dallas Mavericks',
+    primaryColor: '#0063AF',
+    secondaryColor: '#00538C',
+  },
+  {
+    name: 'Denver Nuggets',
+    primaryColor: '#0E2240',
+    secondaryColor: '#FEC524',
+  },
+  {
+    name: 'Detroit Pistons',
+    primaryColor: '#C8102E',
+    secondaryColor: '#006BB6',
+  },
+  {
+    name: 'Golden State Warriors',
+    primaryColor: '#006BB6',
+    secondaryColor: '#FDB927',
+  },
+  {
+    name: 'Houston Rockets',
+    primaryColor: '#CE1141',
+    secondaryColor: '#C4CED4',
+  },
+  {
+    name: 'Indiana Pacers',
+    primaryColor: '#002D62',
+    secondaryColor: '#FDBB30',
+  },
+  {
+    name: 'LA Clippers',
+    primaryColor: '#C8102E',
+    secondaryColor: '#1D42BA',
+  },
+  {
+    name: 'Los Angeles Lakers',
+    primaryColor: '#FDB927',
+    secondaryColor: '#552583',
+  },
+  {
+    name: 'Memphis Grizzlies',
+    primaryColor: '#5D76A9',
+    secondaryColor: '#12173F',
+  },
+  {
+    name: 'Miami Heat',
+    primaryColor: '#98002E',
+    secondaryColor: '#F9A01B',
+  },
+  {
+    name: 'Milwaukee Bucks',
+    primaryColor: '#00471B',
+    secondaryColor: '#EEE1C6',
+  },
+  {
+    name: 'Minnesota Timberwolves',
+    primaryColor: '#0C2340',
+    secondaryColor: '#236192',
+  },
+  {
+    name: 'New Orleans Pelicans',
+    primaryColor: '#0C2340',
+    secondaryColor: '#B4975A',
+  },
+  {
+    name: 'New York Knicks',
+    primaryColor: '#006BB6',
+    secondaryColor: '#F58426',
+  },
+  {
+    name: 'Oklahoma City Thunder',
+    primaryColor: '#007AC1',
+    secondaryColor: '#EF3B24',
+  },
+  {
+    name: 'Orlando Magic',
+    primaryColor: '#0077C9',
+    secondaryColor: '#C4CED4',
+  },
+  {
+    name: 'Philadelphia 76ers',
+    primaryColor: '#006BB6',
+    secondaryColor: '#ED174C',
+  },
+  {
+    name: 'Phoenix Suns',
+    primaryColor: '#1D1160',
+    secondaryColor: '#E56020',
+  },
+  {
+    name: 'Portland Trail Blazers',
+    primaryColor: '#E03A3E',
+    secondaryColor: '#000000',
+  },
+  {
+    name: 'Sacramento Kings',
+    primaryColor: '#5A2D81',
+    secondaryColor: '#63727A',
+  },
+  {
+    name: 'San Antonio Spurs',
+    primaryColor: '#C4CED4',
+    secondaryColor: '#000000',
+  },
+  {
+    name: 'Toronto Raptors',
+    primaryColor: '#CE1141',
+    secondaryColor: '#000000',
+  },
+  {
+    name: 'Utah Jazz',
+    primaryColor: '#002B5C',
+    secondaryColor: '#F9A01B',
+  },
+  {
+    name: 'Washington Wizards',
+    primaryColor: '#002B5C',
+    secondaryColor: '#E21836',
+  },
+];
