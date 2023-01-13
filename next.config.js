8d016fce3b7771084f0c3f9d257c926837572825
Danny Langevin <dklangevin@gmail.com>
2023-01-12 23:20:43 -0700

    Add db config to next config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,24 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   reactStrictMode: true,
   compiler: {
     styledComponents: true,
+  },
+  env: {
+    dbConfig: {
+      database: process.env.DB_NAME,
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      port: process.env.DB_PORT,
+      password: process.env.DB_PW,
+      // ssl: {
+      //   rejectUnauthorized: false,
+      //   key: fs.readFileSync('key.pem'),
+      //   cert: fs.readFileSync('cert.pem'),
+      // },
+      ssl: true,
+    },
   },
 };
 
