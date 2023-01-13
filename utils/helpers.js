beef1655bee61fb9b6b6ac12fd45db4d7ed559e6
Danny Langevin <dklangevin@gmail.com>
2023-01-12 23:20:28 -0700

    Update helper functions

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,14 +1,14 @@
 export const intersection = (arrayA, arrayB) =>
   arrayA.filter((value) => arrayB.includes(value));
 
-export const commonPlayers = (teamIdA, teamIdB, teamPlayers) => {
-  const teamPlayersA = teamPlayers
-    .find((team) => team.team.id === teamIdA)
-    ?.players.map(({ id }) => id);
-  const teamPlayersB = teamPlayers
-    .find((team) => team.team.id === teamIdB)
-    ?.players.map(({ id }) => id);
-  return !teamPlayersA?.length || !teamPlayersB?.length
-    ? []
-    : intersection(teamPlayersA, teamPlayersB);
+export const shuffled = (array) => array.sort(() => 0.5 - Math.random());
+
+export const rowMapping = (mapping, headers) => {
+  const entries = Object.entries(mapping).map(([key, value]) => [
+    key,
+    headers.indexOf(value),
+  ]);
+  console.log(entries);
+  return (row) =>
+    Object.fromEntries(entries.map(([key, value]) => [key, row[value]]));
 };
