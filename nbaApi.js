cfaa63e4a6de98a7bb387e9e02dac185d6e3c613
Danny Langevin <dklangevin@gmail.com>
2023-01-12 23:23:41 -0700

    Update nbaApi.js

--- nbaApi.js
+++ nbaApi.js
@@ -1,4 +1,5 @@
-const axios = require('axios');
+import axios from 'axios';
+import { rowMapping } from './utils/helpers';
 
 const api = axios.create({
   baseURL: 'https://stats.nba.com/stats/',
@@ -14,8 +15,8 @@ const api = axios.create({
   },
 });
 
-export async function getTeams() {
-  const res = await api
+export async function listTeams() {
+  const rows = await api
     .get(
       'leaguestandingsv3?GroupBy=conf&LeagueID=00&Season=2022-23&SeasonType=Regular%20Season&Section=overall'
     )
@@ -24,10 +25,10 @@ export async function getTeams() {
       const rows = data.map((row) => ({ id: row[2], name: row[4] }));
       return rows;
     });
-  return res;
+  return rows;
 }
 
-export async function getPlayers(teamId, historical = false) {
+export async function listTeamPlayers(teamId, historical = false) {
   const options = {
     params: {
       Historical: historical ? '1' : '0',
@@ -46,8 +47,6 @@ export async function getPlayers(teamId, historical = false) {
       console.log(error);
     });
 
-  console.log(data);
-
   const rows = data
     ? data.map((row) => ({
         id: row[0],
@@ -56,6 +55,47 @@ export async function getPlayers(teamId, historical = false) {
         position: row[10],
       }))
     : [];
+
+  return rows;
+}
+
+export async function listPlayers(teamId) {
+  const options = {
+    params: {
+      Historical: '1',
+      LeagueID: '00',
+      Season: '2022-23',
+      SeasonType: 'Regular Season',
+      ...(teamId && { TeamID: teamId }),
+    },
+    timeout: 9000,
+  };
+
+  const [headers, data] = await api
+    .get('playerindex', options)
+    .then((res) => [
+      res.data.resultSets[0].headers,
+      res.data.resultSets[0].rowSet,
+    ])
+    .catch((error) => {
+      console.log(error);
+    });
+
+  const mapping = {
+    id: 'PERSON_ID',
+    firstName: 'PLAYER_FIRST_NAME',
+    lastName: 'PLAYER_LAST_NAME',
+    slug: 'PLAYER_SLUG',
+    teamId: 'TEAM_ID',
+    position: 'POSITION',
+    points: 'PTS',
+    assists: 'AST',
+    rebounds: 'REB',
+  };
+
+  const mappingFunc = rowMapping(mapping, headers);
+
+  const rows = data ? data.map(mappingFunc) : [];
 
   return rows;
 }
