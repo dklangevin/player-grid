5477961a1f147d42709fd0aa3e3de09eb15e6d8e
Danny Langevin <dklangevin@gmail.com>
2023-02-19 01:11:47 -0700

    Try to fix deploy bug again

--- next.config.js
+++ next.config.js
@@ -17,32 +17,36 @@ const nextConfig = {
   },
   async headers() {
     return [
+      // {
+      //   source: '/api/cron/teams',
+      //   headers: [
+      //     {
+      //       key: 'Referer',
+      //       value: 'https://www.nba.com',
+      //     },
+      //   ],
+      // },
       {
-        source: '/api/cron/teams',
+        // matching all API routes
+        source: '/api/:path*',
         headers: [
           {
             key: 'Referer',
             value: 'https://www.nba.com',
           },
+          { key: 'Access-Control-Allow-Credentials', value: 'true' },
+          { key: 'Access-Control-Allow-Origin', value: '*' },
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
+          },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value:
+              'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+          },
         ],
       },
-      // {
-      //   // matching all API routes
-      //   source: '/api/:path*',
-      //   headers: [
-      //     { key: 'Access-Control-Allow-Credentials', value: 'true' },
-      //     { key: 'Access-Control-Allow-Origin', value: '*' },
-      //     {
-      //       key: 'Access-Control-Allow-Methods',
-      //       value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
-      //     },
-      //     {
-      //       key: 'Access-Control-Allow-Headers',
-      //       value:
-      //         'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
-      //     },
-      //   ],
-      // },
     ];
   },
 };
