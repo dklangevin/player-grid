506edbb0dc06a59fd859fcc5044979620853d728
Danny Langevin <dklangevin@gmail.com>
2023-02-19 00:16:36 -0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -12,13 +12,29 @@ const nextConfig = {
       user: process.env.DB_USER,
       port: process.env.DB_PORT,
       password: process.env.DB_PW,
-      // ssl: {
-      //   rejectUnauthorized: false,
-      //   key: fs.readFileSync('key.pem'),
-      //   cert: fs.readFileSync('cert.pem'),
-      // },
       ssl: true,
     },
+  },
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: '/api/:path*',
+        headers: [
+          { key: 'Access-Control-Allow-Credentials', value: 'true' },
+          { key: 'Access-Control-Allow-Origin', value: '*' },
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
+          },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value:
+              'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+          },
+        ],
+      },
+    ];
   },
 };
 
