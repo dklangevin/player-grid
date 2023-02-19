490ed155e4759b22153e22df132c4da272b12322
Danny Langevin <dklangevin@gmail.com>
2023-02-19 12:48:03 -0700

    Update team players api

--- pages/api/teamPlayers.js
+++ pages/api/teamPlayers.js
@@ -1,14 +1,21 @@
 import { Pool } from 'pg';
 import _ from 'lodash';
 
+const LIMIT = 10;
+
 export default async function handler(request, response) {
   const pool = new Pool(process.env.dbConfig);
   const client = await pool.connect();
 
-  const teamsQuery = `SELECT * FROM teams`;
+  // get random list of teams
+  const teamsQuery = `SELECT * FROM teams ORDER BY RANDOM() LIMIT ${LIMIT}`;
   const teamsRows = (await client.query(teamsQuery)).rows;
+  const teamIds = teamsRows.map(({ id }) => id);
 
-  const playersQuery = `SELECT * FROM teamplayers`;
+  // select players from only those teams
+  const playersQuery = `SELECT * FROM teamplayers WHERE teamid IN (${teamIds.join(
+    ','
+  )})`;
   const playersRows = (await client.query(playersQuery)).rows;
 
   client.release();
