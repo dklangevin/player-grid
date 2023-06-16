4e02eb6df5360e591339f351e790633c92bc8b7b
Danny Langevin <dklangevin@gmail.com>
2023-06-16 01:17:38 -0600

    Update nba api

--- nbaApi.js
+++ nbaApi.js
@@ -5,72 +5,27 @@ const api = axios.create({
   baseURL: 'https://stats.nba.com/stats/',
   timeout: 1000,
   headers: {
-    Referer: 'https://www.nba.com',
+    Referer: 'https://www.nba.com/',
     Origin: 'https://www.nba.com',
     Accept: '*/*',
-    // Host: 'stats.nba.com',
-    'Accept-Encoding': 'gzip, deflate, br',
-    'Access-Control-Allow-Origin': '*',
-    // Connection: 'keep-alive',
   },
 });
 
 export async function listTeams() {
-  console.log('fetching teams');
-  const options = {
-    method: 'GET',
-    headers: {
-      Referer: 'https://www.nba.com',
-      // Origin: 'https://www.nba.com',
-      'Referrer-Policy': 'no-referrer',
-      Accept: '*/*',
-    },
-  };
   const rows = await api
     .get(
-      'stats/leaguestandingsv3?GroupBy=conf&LeagueID=00&Season=2022-23&SeasonType=Regular%20Season&Section=overall'
+      'leaguestandingsv3?GroupBy=conf&LeagueID=00&Season=2022-23&SeasonType=Regular%20Season&Section=overall'
     )
     .then((res) => {
       const data = res.data.resultSets[0].rowSet;
       const rows = data.map((row) => ({ id: row[2], name: row[4] }));
+      console.log('teams fetch success');
       return rows;
     })
     .catch((err) => {
       console.log(err);
-      console.log(err.request._header);
+      console.log('teams fetch error');
     });
-  console.log('teams fetch success');
-  return rows;
-}
-
-export async function listTeamPlayers(teamId, historical = false) {
-  const options = {
-    params: {
-      Historical: historical ? '1' : '0',
-      LeagueID: '00',
-      Season: '2022-23',
-      SeasonType: 'Regular Season',
-      TeamID: teamId,
-    },
-    timeout: 5000,
-  };
-
-  const data = await api
-    .get('playerindex', options)
-    .then((res) => res.data.resultSets[0].rowSet)
-    .catch((error) => {
-      console.log(error);
-    });
-
-  const rows = data
-    ? data.map((row) => ({
-        id: row[0],
-        name: `${row[2]} ${row[1]}`,
-        number: row[9],
-        position: row[10],
-      }))
-    : [];
-
   return rows;
 }
 
@@ -112,27 +67,5 @@ export async function listPlayers(teamId) {
 
   const rows = data ? data.map(mappingFunc) : [];
 
-  return rows;
-}
-
-export async function listTeam() {
-  console.log('fetching team');
-  const rows = await api
-    .get(
-      'playerindex?College=&Country=&DraftPick=&DraftRound=&DraftYear=&Height=&Historical=0&LeagueID=00&Season=2022-23&SeasonType=Regular Season&TeamID=1610612757&Weight='
-    )
-    .then((res) => {
-      console.log(res.data);
-      const data = res.data.resultSets[0].rowSet;
-      const rows = data.map((row) => ({ id: row[2], name: row[4] }));
-      console.log('team fetch success');
-
-      return rows;
-    })
-    .catch((err) => {
-      console.log(err.message);
-      console.log(err.request._header);
-      console.log('team fetch error');
-    });
   return rows;
 }
