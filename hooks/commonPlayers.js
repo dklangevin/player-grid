e33655ba2a0abd10fecd11ac2e3054ecac947704
Danny Langevin <dklangevin@gmail.com>
2023-06-16 01:17:46 -0600

    Update hooks and helpers

--- hooks/commonPlayers.js
+++ hooks/commonPlayers.js
@@ -1,17 +1,13 @@
 import { useMemo } from 'react';
-import { commonPlayers, intersection } from '../utils/helpers';
+import { intersection } from '../utils/helpers';
 
 export default function useCommonPlayers(rowTeams, colTeams) {
   return useMemo(() => {
-    const [m, n] = [rowTeams?.length, colTeams?.length];
-    if (!m || !n) {
-      return null;
-    }
-    return rowTeams.map((_, i) =>
-      colTeams.map((_, j) =>
+    return rowTeams?.map((_, i) =>
+      colTeams?.map((_, j) =>
         intersection(
-          rowTeams[i].players.map(({ id }) => id),
-          colTeams[j].players.map(({ id }) => id)
+          rowTeams[i].teamPlayers.map(({ playerId }) => playerId),
+          colTeams[j].teamPlayers.map(({ playerId }) => playerId)
         )
       )
     );
--- utils/helpers.js
+++ utils/helpers.js
@@ -16,6 +16,42 @@ export const uniqueBy = (array, key) => {
   const seen = new Set();
   return array.filter((item) => {
     const k = item[key];
-    return seen.has(k) ? false : seen.add(k);
+    if (seen.has(k)) {
+      return false;
+    } else {
+      seen.add(k);
+      return true;
+    }
   });
+};
+
+export const generateSeed = () => {
+  let seed = '';
+  for (let i = 0; i < 10; i++) {
+    const val = Math.floor(Math.random() * 32).toString(32);
+    seed = `${seed}${val}`;
+  }
+  console.log(seed);
+  return seed;
+};
+
+export const decodeSeed = (seed) => {
+  const arr = [];
+  for (let i = 0; i < 10; i++) {
+    const val = seed.substring(i, i + 1);
+    arr.push(parseInt(val, 32));
+  }
+};
+
+export const getQuery = (query) => {
+  if (!query) {
+    return '';
+  }
+  const queryString = Object.entries(query)
+    .map((item) => (item[1] !== undefined ? `${item[0]}=${item[1]}` : ''))
+    .join('&');
+  if (queryString) {
+    return `?${queryString}`;
+  }
+  return '';
 };
